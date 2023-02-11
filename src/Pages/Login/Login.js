8a67a62d1c56cb2a06ba42719ca0ef73d7035f58
Nirob Hasan <nirob427417@gmail.com>
2023-02-11 11:05:39 +0600

    firebase deploy

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -59,7 +59,7 @@ const Login = () => {
                 console.log(currentUser);
 
                 // get jwt token
-                fetch('http://localhost:5000/jwt', {
+                fetch('https://genius-car-server-xi-gray.vercel.app/jwt', {
                     method: 'POST',
                     headers: {
                         'content-type': 'application/json'
